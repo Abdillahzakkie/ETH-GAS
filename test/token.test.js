ac0fae6c85265271115226db3b4c066b92aa18cd
Abdillahzakkie <zakariyyaopeyemi@gmail.com>
2020-11-16 15:43:29 +0100

    made some chnages

--- test/token.test.js
+++ test/token.test.js
@@ -7,7 +7,17 @@ const { ZERO_ADDRESS } = require( "@openzeppelin/test-helpers/src/constants" );
 const toWei = _amount => web3.utils.toWei(_amount.toString(), 'ether');
 
 contract('Token', async ([admin, user1]) => {
-    beforeEach(async () => this.token = await Token.new("GasToken", "GTX", { from: admin }));
+    let timestamps = [];
+    let gasUsed = [];
+
+    beforeEach(async () => {
+        this.token = await Token.new("GasToken", "GTX", { from: admin });
+        for(let i = 1; i <= 5; ++i) {
+            const currentTime = Date.now();
+            timestamps = [timestamps, currentTime];
+            gasUsed = [...gasUsed, i];
+        }
+    });
 
     it('should set token details properly', async () => {
         const name = await this.token.name();
@@ -17,5 +27,11 @@ contract('Token', async ([admin, user1]) => {
         expect(name).to.equal("GasToken");
         expect(symbol).to.equal("GTX");
         expect(totalSupply.toString()).to.equal(toWei(7000));
+    })
+
+    describe('Claim rewards', () => {
+        it("should claim rewards properly", async () => {
+            await this.token.claimRewards(timestamps, gasUsed);
+        })
     })
 })
