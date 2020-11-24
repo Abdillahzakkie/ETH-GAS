6e0a260534c5782a198ce0eaccfe91f1f90ebee4
Abdillahzakkie <zakariyyaopeyemi@gmail.com>
2020-11-24 18:43:16 +0100

    made some changes

--- contracts/V1/EthanolVaultUpgrade.sol
+++ contracts/V1/EthanolVaultUpgrade.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.4.22 <0.8.0;
 import "@openzeppelin/contracts-ethereum-package/contracts/Initializable.sol";
 
 
-contract EthanolVaultUpgraded {
+contract EthanolVaultUpgraded is Initializable {
     address public wallet;
 
     function initialize() public initializer {
